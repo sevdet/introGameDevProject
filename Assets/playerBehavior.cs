f453b4b88ba7da3c9096e1fbec06b8a9c3cfa9f7
Unknown <snt265@nyu.edu>
2018-02-08 00:59:45 -0500

    changed playerBehavior script and deleted wall script
    player movement implemented in a better way for smoother movement and wall script going to be redone

--- Assets/playerBehavior.cs
+++ Assets/playerBehavior.cs
@@ -4,46 +4,20 @@ using UnityEngine;
 
 public class playerBehavior : MonoBehaviour {
 
-	public Rigidbody2D playerRB;
-	SpriteRenderer playerSprite;
-	public float speed;
-	Vector2 direction;
-	Vector2 movement;
-	public Vector2 playerStartingPosition;
+	private float speed;
 
 	// Use this for initialization
 	void Start () {
-		playerRB = this.GetComponent<Rigidbody2D> ();
-		playerSprite = this.GetComponent<SpriteRenderer> ();
-		speed = 4;
+		speed = 6;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		move ();
 
-	}
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
 
-	void move() {
-		direction = Vector2.zero;
-
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-			direction.x = 1;
-		}
-
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-			direction.x = -1;
-		}
-
-		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
-			direction.y = 1;
-		}
-
-		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-			direction.y = -1;
-		}
-
-		transform.Translate(direction * speed * Time.deltaTime);
+		transform.Translate (new Vector2 (xAxis, yAxis)* Time.deltaTime*speed);
 	}
 		
 }
