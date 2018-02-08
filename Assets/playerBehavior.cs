2fd91c46f7a16f1d24d91bd001f91ecf93dfe689
Unknown <snt265@nyu.edu>
2018-02-08 10:02:57 -0500

    wall collision working now
    no separate wall script, just edited playerBehavior

--- Assets/playerBehavior.cs
+++ Assets/playerBehavior.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class playerBehavior : MonoBehaviour {
 
 	private float speed;
+	private float xAxis;
+	private float yAxis;
+	private bool moving;
+	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		speed = 6;
+
+		speed = 4;
+		rb = this.GetComponent<Rigidbody2D> ();
+		rb.bodyType = RigidbodyType2D.Dynamic;
+		rb.gravityScale = 0;
+		rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		xAxis = Input.GetAxis ("Horizontal");
+		yAxis = Input.GetAxis ("Vertical");
 
-		float xAxis = Input.GetAxis ("Horizontal");
-		float yAxis = Input.GetAxis ("Vertical");
-
-		transform.Translate (new Vector2 (xAxis, yAxis)* Time.deltaTime*speed);
+		transform.Translate (new Vector2 (xAxis, yAxis) * Time.deltaTime * speed);
 	}
 		
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "prototypeMazeWall") {
+			Debug.Log ("collided with wall");
+		}
+	}
+
 }
