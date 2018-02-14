c09eb7cedcec9267225285df19d09d3bc004d3b9
Unknown <snt265@nyu.edu>
2018-02-14 11:22:59 -0500

    wall prefabs
    made two hallway prefabs and starting to design maze to test

---
+++ Assets/enemyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehavior : MonoBehaviour {
+
+	public Transform target;//set target from inspector instead of looking in Update
+	public float speed = 6;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// rotate enemy to face player
+		transform.LookAt(target.position);
+		transform.Rotate(new Vector3(0,-90,0),Space.Self);
+
+		// translating enemy towards position of player FOR NOW
+		// going to eventually make the target be the player's last point of collision w the wall
+		if (Vector3.Distance(transform.position,target.position) > 0.8f){
+			transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
+		}
+	}
+
+}
--- Assets/playerBehavior.cs
+++ Assets/playerBehavior.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class playerBehavior : MonoBehaviour {
 
 	private float speed;
-	private float xAxis;
-	private float yAxis;
-	private bool moving;
+	private float xAxis; // move right when 1 move left when -1
+	private float yAxis; // move up when 1 move down when -1
+	private Vector2 pointOfCollision; // point the player collides with a wall so enemy can follow this point
 	public Rigidbody2D rb;
 
 	// Use this for initialization
@@ -15,24 +15,24 @@ public class playerBehavior : MonoBehaviour {
 
 		speed = 4;
 		rb = this.GetComponent<Rigidbody2D> ();
-		rb.bodyType = RigidbodyType2D.Dynamic;
-		rb.gravityScale = 0;
-		rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+		rb.bodyType = RigidbodyType2D.Dynamic; 
+		rb.gravityScale = 0; // want collision but top down so no gravity
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		xAxis = Input.GetAxis ("Horizontal");
-		yAxis = Input.GetAxis ("Vertical");
+		xAxis = Input.GetAxis ("Horizontal"); // handle direction for A/D/left/right key presses
+		yAxis = Input.GetAxis ("Vertical"); // handle direction for W/S/up/down key presses 
 
-		transform.Translate (new Vector2 (xAxis, yAxis) * Time.deltaTime * speed);
+		transform.Translate (new Vector2 (xAxis, yAxis) * Time.deltaTime * speed); 
 	}
 		
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.name == "prototypeMazeWall") {
+		if (coll.gameObject.tag == "wall") {
 			Debug.Log ("collided with wall");
+			pointOfCollision = rb.transform.position;
 		}
 	}
 
